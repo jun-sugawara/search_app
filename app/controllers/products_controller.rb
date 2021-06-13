060fcebeda37e13dbc640d5d18ccab774450af77
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-06-13 19:38:52 +0900

    コントローラー内のメソッドを記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,19 @@
 class ProductsController < ApplicationController
+
+  before_action :search_product, only: [:index, :search]
+
   def index
+    @products = Product.all
   end
 
   def search
+    @reaults = @p.result.includes(:category)
   end
+
+  private
+
+  def search_products
+    @p = Product.ransack(params[:q])
+  end
+
 end
